eae08196231e7f1f435b75101f9b594e7016f7e9
liamjbennett <lbennett@opentable.com>
2014-03-20 15:42:16 +0000

    some more tweaks to my machine

--- modules/people/manifests/liamjbennett.pp
+++ modules/people/manifests/liamjbennett.pp
@@ -4,6 +4,7 @@ class people::liamjbennett {
   include alfred
   include caffeine
   include keepassx
+  include python
   
   # standard office tools
   include chrome
@@ -14,13 +15,25 @@ class people::liamjbennett {
   # dev tools
   include iterm2::stable
   include vagrant
+  include packer
   include vmware_fusion
-  include intellij
+  include virtualbox
+  #class { 'intellij':
+  #  edition => 'ultimate',
+  #  version => '12.1.3'
+  #}
   include mou
+  #include royaltsk
+ 
+  include python
   include vim
-  include dotfiles
+  #include dotfiles
+  include textmate::textmate2::beta
 
   # personal stuff
   include vlc
   include onepassword
+  include wunderlist
+  include evernote
+  include spotify
 }
