c4ff4309375eebbe546a31697db7a56e7aae8204
Seif Attar <iam@seifattar.net>
2014-04-11 18:13:35 +0100

    Add homesick to Seifs manifest

--- modules/people/manifests/seif.pp
+++ modules/people/manifests/seif.pp
@@ -1,4 +1,7 @@
 class people::seif{
+
+  $home = "/Users/${::boxen_user}"
+
   include alfred
   include iterm2::stable
   include alfred
@@ -21,5 +24,68 @@ class people::seif{
   include mongodb::client
   include vim
   include webstorm
+
+  package {
+    [
+      'ttytter',
+      'tmux'
+    ]:
+    ensure => present,
+  }
+
+  #Install homesick for managing dotfiles
+  package { 'homesick':
+    ensure   => 'installed',
+    provider => 'gem',
+  }
+
+  # Get repos and link files
+  repository { 'homesick-bash':
+    source => 'seif/homesick-bash',
+    path => "${home}/.homesick/repos/homeshick-bash"
+  }
+  ~> exec { "homesick symlink homesick-bash --force":
+    refreshonly => true
+  }
+
+  repository { 'dotfiles':
+    source => 'seif/homesick-dotfiles',
+    path => "${home}/.homesick/repos/homesick-dotfiles"
+  }
+  ~> exec { "homesick symlink dotfiles --force":
+    refreshonly => true
+  }
+
+  repository { 'homesick-private':
+    source => 'seif/homesick-private',
+    path => "${home}/.homesick/repos/homeshick-private"
+  }
+  ~> exec { "homesick symlink homesick-private --force":
+    refreshonly => true
+  }
+
+  repository { 'homesick-tmux':
+    source => 'seif/homesick-tmux',
+    path => "${home}/.homesick/repos/homeshick-tmux"
+  }
+  ~> exec { "homesick symlink homesick-tmux --force":
+    refreshonly => true
+  }
+
+  repository { 'homesick-ttytter':
+    source => 'seif/homesick-ttytter',
+    path => "${home}/.homesick/repos/homeshick-ttytter"
+  }
+  ~> exec { "homesick symlink homesick-ttytter --force":
+    refreshonly => true
+  }
+
+  repository { 'homesick-vim':
+    source => 'seif/homesick-vim',
+    path => "${home}/.homesick/repos/homeshick-vim"
+  }
+  ~> exec { "homesick symlink homesick-vim --force":
+    refreshonly => true
+  }
 }
 
