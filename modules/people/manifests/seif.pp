0f9c757298e7f55b4187232f5631a2f83db23862
Seif Attar <iam@seifattar.net>
2014-04-11 17:27:07 +0100

    Add manifestfile for Seif

---
+++ modules/people/manifests/seif.pp
@@ -0,0 +1,25 @@
+class people::seif{
+  include alfred
+  include iterm2::stable
+  include alfred
+
+  include firefox
+  include chrome
+  include sparrow
+
+  include vlc
+  include spotify
+
+  include dropbox
+  include skype
+  include hipchat
+  include colloquy
+  include wunderlist
+
+  include vagrant
+  include packer
+  include mongodb::client
+  include vim
+  include webstorm
+}
+
