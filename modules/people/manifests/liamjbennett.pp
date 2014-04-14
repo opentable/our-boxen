811fe495179e5a5e3d723141ffb3aa012f46c461
liamjbennett <lbennett@opentable.com>
2014-04-14 13:03:10 +0100

    lots of changes

--- modules/people/manifests/liamjbennett.pp
+++ modules/people/manifests/liamjbennett.pp
@@ -1,16 +1,32 @@
 class people::liamjbennett {
 
   # system tools
-  include alfred
   include caffeine
   include keepassx
   include python
+  include cyberduck
+  include colloquy
+  include unarchiver
+  include adobeflashplayer::debugger
+ 
+  package { 'huffshell':
+    ensure   => present,
+    provider => gem
+  }
+  
+  package { 'wget':
+    ensure   => present,
+    provider => homebrew
+  }
   
   # standard office tools
   include chrome
+  #include firefox
   include dropbox
   include skype
   include hipchat
+  # office 2011
+  # vpn
 
   # dev tools
   include iterm2::stable
@@ -18,17 +34,28 @@ class people::liamjbennett {
   include packer
   include vmware_fusion
   include virtualbox
-  #class { 'intellij':
-  #  edition => 'ultimate',
-  #  version => '12.1.3'
+  
+  class { 'intellij':
+    edition => 'ultimate',
+    version => '13.0.3'
+  }
+  
+  #class { 'royaltsx':
+  #  version => '1.3.2'
   #}
-  include mou
-  #include royaltsk
  
-  include python
   include vim
-  #include dotfiles
   include textmate::textmate2::beta
+  
+  # dotfiles - TODO: pull this out into it's own module
+  #$home = "/Users/${::boxen_user}"
+  #$dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
+
+  #repository { $dotfiles_dir:
+  #  source => "liamjbennett/dotfiles"
+  #}
+  
+  #include vundle - TODO: write me
 
   # personal stuff
   include vlc
@@ -36,4 +63,5 @@ class people::liamjbennett {
   include wunderlist
   include evernote
   include spotify
+  #include tweetdeck
 }
